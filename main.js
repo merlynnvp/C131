5c2569e3c8ddacff4f484a135b76d11b41c21107
merlynnvp <72741790+merlynnvp@users.noreply.github.com>
2021-06-20 15:46:40 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 img = "";
+status = "";
+object = [];
 
 function preload()
 {
@@ -9,14 +11,43 @@ function setup()
 {
     canvas = createCanvas(640, 420);
     canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "status : Detecting Objects";
+}
+
+function modelLoaded()
+{
+console.log;
+status = true;
+objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if(error) 
+    {
+        console.log(error);
+    }
+    else
+    console.log(results);
+    objects = results;
 }
 
 function draw()
 {
     image(img, 0, 0, 640, 420);
-    fill("#FF0000");
-    text("Dog", 45, 75);
-    noFill("#FF0000");
-    stroke("#FF0000");
-    rect(30, 60, 450, 350 );
+    if(status !="")
+    {
+      for(i = 0; i < objects.length; i++)
+        {
+          document.getElementById("status").innerHTML = "Status : Object Detected";
+
+          fill("#FF0000");
+          percent = floor(objects[i].confidence * 100);
+          text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+          noFill();
+          stroke("#FF0000");
+          rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
 }
